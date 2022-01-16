60eb7a8db0faf64aab25bc488200a0bba8e2648d
Djalmir <djalmir_miodutzki@hotmail.com>
2022-01-16 16:04:04 -0300

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/TaskBoard/sw.js')
+	navigator.serviceWorker.register('./TaskBoard/sw.js')
 }
 
 let user = JSON.parse(localStorage.getItem('Razion.user'))
